2834b80e9a10ce1d1e94618b2f11822139ddb9d9
Cipher58 <jackfranklin899@gmail.com>
2024-02-20 00:40:06 +0000

    Automation

--- Installers/Launcher.cpp
+++ Installers/Launcher.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Execute terminal command
-    system("cd ~/.NLU-Application && python3 main.py");
+    system("cd ~/.Janex-Assistant-Frontend && python3 main.py");
 
     return 0;
 }
